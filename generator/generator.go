dacd30c0dfcbe1cfdc0da2228c2f5f19c590703d
Jorge Rosas <rosasjorge1701@gmail.com>
2023-09-17 00:09:05 -0300

    Refactoring parser

--- generator/generator.go
+++ generator/generator.go
@@ -27,30 +27,14 @@ func (g *Generator) Generate() []byte {
 	textBuff.WriteString("global _start\n")
 	textBuff.WriteString("_start:\n")
 
-
-	for _, s := range g.nodeProg.Statements {
+	for _, s := range g.nodeProg.Stmts {
 		fmt.Println(s)
-		switch stmt := s.(type) {
-		case *parser.NodeInitialize:
-			fmt.Println("NodeInitialize")
-			dataBuff.WriteString(fmt.Sprintf("    %s DD %s\n", stmt.Identifier.Value, stmt.Literal.Value))
-		case *parser.NodeSalirLiteral:
-			fmt.Println("NodeSalirLiteral")
-			textBuff.WriteString("    mov rax, 60\n")
-			textBuff.WriteString(fmt.Sprintf("    mov rdi,%s\n", stmt.Literal.Value))
-			textBuff.WriteString("    syscall\n")
-		case *parser.NodeSalirIdentifier:
-			fmt.Println("NodeSalirIdentifier")
-			textBuff.WriteString("    mov rax, 60\n")
-			textBuff.WriteString(fmt.Sprintf("    mov rdi,[%s]\n", stmt.Identifier.Value))
-			textBuff.WriteString("    syscall\n")
-		}
 	}
 
 	dataBuff.WriteString("\n")
 	textBuff.WriteString("\n")
 
-    // Exit without error at the end, if no other exit then this prevents segmentation fault
+	// Exit without error at the end, if no other exit then this prevents segmentation fault
 	textBuff.WriteString("    mov rax, 60\n")
 	textBuff.WriteString("    mov rdi,0\n")
 	textBuff.WriteString("    syscall\n")
--- parser/parser.go
+++ parser/parser.go
@@ -2,28 +2,49 @@ package parser
 
 import (
 	"compiler/tokenizer"
+	"fmt"
+	"log"
 )
 
-type NodeExprLiteral struct {
-	Literal *tokenizer.Token
+const (
+	NodeTypeExprLit byte = iota
+	NodeTypeExprIdent
+	NodeTypeExprOper
+
+	NodeTypeStmtInit
+	NodeTypeStmtExit
+)
+
+type NodeProg struct {
+	Stmts []NodeStmt
 }
 
-type NodeExprIdentifier struct {
-	Identifier *tokenizer.Token
+type NodeExpr struct {
+	t     byte
+	Lit   *tokenizer.Token
+	Ident *tokenizer.Token
+	Oper  *NodeOper
 }
 
-type NodeExprOperation[nodeExpr NodeExpr] struct {
-	LeftExpr  nodeExpr
-	Operator  *tokenizer.Token
-	RightExpr nodeExpr
+type NodeOper struct {
+	Oper *tokenizer.Token
+	Lhs  *NodeExpr
+	Rhs  *NodeExpr
 }
 
-type NodeExpr interface {
-	NodeExprLiteral | NodeExprIdentifier
+type NodeStmt struct {
+	t    byte
+	Init *NodeStmtInit
+	Exit *NodeStmtExit
 }
 
-type NodeExit[T NodeExpr] struct {
-	Expr T
+type NodeStmtInit struct {
+	Ident *tokenizer.Token
+	Expr  *NodeExpr
+}
+
+type NodeStmtExit struct {
+	Expr *NodeExpr
 }
 
 type Parser struct {
@@ -31,44 +52,135 @@ type Parser struct {
 	index  int
 }
 
-func NewParser(tokens []tokenizer.Token) *Parser {
-	return &Parser{
-		tokens: tokens,
+func (p *Parser) GenerateNodes() (*NodeProg, error) {
+	nodeProg := &NodeProg{
+		Stmts: []NodeStmt{},
 	}
-}
 
-func (p *Parser) Parse() *NodeProg {
-	return nil // Refactoring...
-}
-
-func (p *Parser) matchEqual(tokenType tokenizer.TokenType) bool {
-	return p.tokens[p.index].Type == tokenType
-}
-
-func (p *Parser) matchAny(tokens ...tokenizer.TokenType) bool {
-	for _, t := range tokens {
-		if p.tokens[p.index].Type == t {
-			return true
+	for p.hasToken() {
+		if p.match(tokenizer.INT) {
+			node, err := p.parseNodeStmtInit()
+			if err != nil {
+				return &NodeProg{}, err
+			}
+			nodeProg.Stmts = append(nodeProg.Stmts, node)
+		} else if p.match(tokenizer.EXIT) {
+			node, err := p.parseNodeStmtExit()
+			if err != nil {
+				return &NodeProg{}, err
+			}
+			nodeProg.Stmts = append(nodeProg.Stmts, node)
+		} else if p.match(tokenizer.SEP) {
+			p.consume()
+		} else {
+			return &NodeProg{}, fmt.Errorf("Error in parsing, cannot parse token '%s'", p.peek().String())
 		}
 	}
-	return false
+	return nodeProg, nil
 }
 
-func (p *Parser) matchSequence(tokens ...tokenizer.TokenType) bool {
-	for i, t := range tokens {
-		if p.tokens[p.index+i].Type != t {
-			return false
+func (p *Parser) parseNodeStmtInit() (NodeStmt, error) {
+	if p.match(tokenizer.INT, tokenizer.IDENTIFIER, tokenizer.EQ) {
+		p.consume()
+		ident := p.consume()
+		p.consume()
+
+		expr, err := p.parseNodeExpr()
+		if err != nil {
+			return NodeStmt{}, err
+		}
+		return NodeStmt{
+			t: NodeTypeStmtInit,
+			Init: &NodeStmtInit{
+				Ident: ident,
+				Expr:  &expr,
+			},
+		}, nil
+	}
+	return NodeStmt{}, fmt.Errorf("Error parsing initialization statement")
+}
+
+func (p *Parser) parseNodeStmtExit() (NodeStmt, error) {
+	if p.match(tokenizer.EXIT) {
+		p.consume()
+		expr, err := p.parseNodeExpr()
+		if err != nil {
+			return NodeStmt{}, err
+		}
+		return NodeStmt{
+			t: NodeTypeStmtExit,
+			Exit: &NodeStmtExit{
+				Expr: &expr,
+			},
+		}, nil
+	}
+	return NodeStmt{}, fmt.Errorf("Error parsing exit statement")
+}
+
+func (p *Parser) parseNodeExpr() (NodeExpr, error) {
+	var tokens []tokenizer.Token
+
+	for !p.match(tokenizer.SEP) {
+		if p.match(tokenizer.LITERAL, tokenizer.IDENTIFIER, tokenizer.ADD, tokenizer.SUB, tokenizer.MUL, tokenizer.DIV) {
+			tokens = append(tokens, *p.consume())
+		} else {
+			return NodeExpr{}, fmt.Errorf("Error parsing expresion, invalid token '%s'", p.tokens[p.index].String())
 		}
 	}
-	return true
+
+	if len(tokens) == 1 {
+		if tokens[0].Type == tokenizer.LITERAL {
+			return NodeExpr{
+				t:   NodeTypeExprLit,
+				Lit: &tokens[0],
+			}, nil
+		} else if tokens[0].Type == tokenizer.IDENTIFIER {
+			return NodeExpr{
+				t:     NodeTypeExprIdent,
+				Ident: &tokens[0],
+			}, nil
+		} else {
+			return NodeExpr{}, fmt.Errorf("Error parsing expresion, invalid token '%s'", tokens[0].String())
+		}
+	} else if len(tokens) == 0 {
+		return NodeExpr{}, fmt.Errorf("Error parsing expresion, no tokens found")
+	}
+
+	oper, err := parseNodeOper(tokens)
+	if err == nil {
+		return NodeExpr{
+			t:    NodeTypeExprOper,
+			Oper: &oper,
+		}, nil
+	}
+
+	return NodeExpr{}, fmt.Errorf("Error parsing expresion")
+}
+
+func parseNodeOper(tokens []tokenizer.Token) (NodeOper, error) {
+	// TODO
+	return NodeOper{}, nil
 }
 
 func (p *Parser) hasToken() bool {
 	return p.index < len(p.tokens)
 }
 
-func (p *Parser) consume() tokenizer.Token {
+func (p *Parser) peek() *tokenizer.Token {
+	return &p.tokens[p.index]
+}
+
+func (p *Parser) match(tokenTypes ...tokenizer.TokenType) bool {
+	for i, tt := range tokenTypes {
+		if i > len(p.tokens) || p.tokens[i].Type != tt {
+			return false
+		}
+	}
+	return true
+}
+
+func (p *Parser) consume() *tokenizer.Token {
 	t := p.tokens[p.index]
 	p.index++
-	return t
+	return &t
 }

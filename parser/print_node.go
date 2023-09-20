57cbc04a6207dcde209a1de42e8697148646d233
Jorge Rosas <rosasjorge1701@gmail.com>
2023-09-19 21:35:38 -0300

    Changed print format

--- parser/print_node.go
+++ parser/print_node.go
@@ -1,57 +1,90 @@
 package parser
 
 import (
+	"compiler/tokenizer"
 	"fmt"
-	"strings"
 )
 
 func (n NodeProg) Print() {
-	fmt.Println("+ NodeProg")
-	for _, stmt := range n.Stmts {
-		fmt.Printf("|")
-		stmt.Print(0)
+	fmt.Println("─ NodeProg")
+	for i, stmt := range n.Stmts {
+		var indent string
+		last := i == len(n.Stmts)-1
+		if last {
+			indent = "  "
+		} else {
+			indent = "  "
+		}
+		stmt.Print(indent, last)
 	}
 }
 
-func (stmt NodeStmt) Print(offset int) {
+func (stmt NodeStmt) Print(indent string, last bool) {
+	indent = pIndent(indent, last)
 	switch stmt.t {
 	case TypeNodeStmtInit:
-		fmt.Printf("- NodeTypeStmtInit {Ident: '%s'}\n", stmt.Init.Ident.Value)
-		stmt.Init.Expr.Print(offset + 1)
+		fmt.Println("NodeTypeStmtInit")
+		printToken(stmt.Init.Ident, indent, false)
+		stmt.Init.Expr.Print(indent, true)
 	case TypeNodeStmtExit:
-		fmt.Printf("- NodeTypeStmtExit\n")
-		stmt.Exit.Expr.Print(offset + 1)
+		fmt.Println("NodeTypeStmtExit")
+		stmt.Exit.Expr.Print(indent, true)
 	default:
 		fmt.Println("NOT IMPLEMENTED NodeStmt.Print")
 	}
 }
 
-func (expr NodeExpr) Print(offset int) {
+func (expr NodeExpr) Print(indent string, last bool) {
 	switch expr.t {
 	case TypeNodeExprTerm:
-		expr.Term.Print(offset + 1)
+		expr.Term.Print(indent, last)
 	case TypeNodeExprOper:
-		expr.Oper.Print(offset + 1)
+		expr.Oper.Print(indent, last)
 	default:
+		if last {
+			fmt.Println("\\-")
+		} else {
+			fmt.Println("|-")
+		}
 		fmt.Println("NOT IMPLEMENTED NodeExpr.Print")
 	}
 }
 
-func (term NodeTerm) Print(offset int) {
-	fmt.Printf("|%s- ", strings.Repeat(" ", offset))
+func (term NodeTerm) Print(indent string, last bool) {
+	indent = pIndent(indent, last)
 	switch term.t {
 	case TypeNodeTermLit:
-		fmt.Printf("NodeTypeTermLit {Lit: '%s'}\n", term.Lit.Value)
+		fmt.Println("NodeTypeTermLit")
+		printToken(term.Lit, indent, true)
 	case TypeNodeTermIdent:
-		fmt.Printf("NodeTypeTermIdent {Ident: '%s'}\n", term.Ident.Value)
+		fmt.Println("NodeTypeTermIdent")
+		printToken(term.Ident, indent, true)
 	default:
 		fmt.Println("NOT IMPLEMENTED NodeTerm.Print")
 	}
 }
 
-func (oper NodeOper) Print(offset int) {
-	fmt.Printf("|%s- ", strings.Repeat(" ", offset))
-	fmt.Printf("NodeOper {Oper: '%s'}\n", oper.Oper.Value)
-	oper.Lhs.Print(offset + 1)
-	oper.Rhs.Print(offset + 1)
+func (oper NodeOper) Print(indent string, last bool) {
+	indent = pIndent(indent, last)
+	fmt.Println("NodeOper")
+	printToken(oper.Oper, indent, false)
+	oper.Lhs.Print(indent, false)
+	oper.Rhs.Print(indent, true)
+}
+
+func pIndent(indent string, last bool) string {
+	fmt.Printf(indent)
+	if last {
+		fmt.Printf("└─ ")
+		indent += "   "
+	} else {
+		fmt.Printf("├─ ")
+		indent += "│  "
+	}
+	return indent
+}
+
+func printToken(token *tokenizer.Token, indent string, last bool) {
+	indent = pIndent(indent, last)
+	fmt.Printf("Token %s\n", token.String())
 }

660a1c07c2e796ad1f4b11e2d09849009c2e5c1a
Jorge Rosas <rosasjorge1701@gmail.com>
2023-09-28 22:35:42 -0300

    Added scopes (#13)
    * Added scopes

--- parser/statement.go
+++ parser/statement.go
@@ -8,6 +8,7 @@ import (
 const (
 	TypeNodeStmtInit = iota
 	TypeNodeStmtReassign
+	TypeNodeStmtScope
 	TypeNodeStmtExit
 )
 
@@ -15,6 +16,7 @@ type NodeStmt struct {
 	T        byte
 	Init     *NodeStmtInit
 	Reassign *NodeStmtReassign
+	Scope    *NodeStmtScope
 	Exit     *NodeStmtExit
 }
 
@@ -28,8 +30,39 @@ type NodeStmtReassign struct {
 	Expr  *NodeExpr
 }
 
+type NodeStmtScope struct {
+	Scope *NodeScope
+}
+
 type NodeStmtExit struct {
 	Expr *NodeExpr
+}
+
+func (p *Parser) parseNodeStmt() (node NodeStmt, err error) {
+	if p.peek().IsType(tokenizer.INT) {
+		node, err = p.parseNodeStmtInit()
+		if err != nil {
+			return NodeStmt{}, err
+		}
+	} else if p.peek().IsType(tokenizer.IDENTIFIER) {
+		node, err = p.parseNodeStmtReassign()
+		if err != nil {
+			return NodeStmt{}, err
+		}
+	} else if p.peek().IsType(tokenizer.B_L) {
+		node, err = p.parseNodeStmtScope()
+		if err != nil {
+			return NodeStmt{}, err
+		}
+	} else if p.peek().IsType(tokenizer.EXIT) {
+		node, err = p.parseNodeStmtExit()
+		if err != nil {
+			return NodeStmt{}, err
+		}
+	} else {
+		return NodeStmt{}, fmt.Errorf("Cannot parse statement")
+	}
+	return node, nil
 }
 
 func (p *Parser) parseNodeStmtInit() (NodeStmt, error) {
@@ -71,6 +104,19 @@ func (p *Parser) parseNodeStmtReassign() (NodeStmt, error) {
 		}, nil
 	}
 	return NodeStmt{}, fmt.Errorf("Error parsing reassignment statement")
+}
+
+func (p *Parser) parseNodeStmtScope() (NodeStmt, error) {
+	scope, err := p.parseNodeScope()
+	if err != nil {
+		return NodeStmt{}, err
+	}
+	return NodeStmt{
+		T: TypeNodeStmtScope,
+		Scope: &NodeStmtScope{
+			Scope: &scope,
+		},
+	}, nil
 }
 
 func (p *Parser) parseNodeStmtExit() (NodeStmt, error) {
